a3e4ac29463fcb8de2ac3d87e05416cd25f964f7
Thiago Campos de Oliveira <thiago.oliveira@gmail.com>
2016-07-06 19:05:45 -0300

    Aula 1

---
+++ Financas/Entidades/Tipo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Financas.Entidades
+{
+    public enum Tipo
+    {
+
+        Entrada, Saida
+
+    }
+}
