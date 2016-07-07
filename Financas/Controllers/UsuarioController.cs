e4e2930e978f767309383acfa6e63d249cc6bcc8
Thiago Campos de Oliveira <thiago.oliveira@gmail.com>
2016-07-07 19:07:57 -0300

    Aula 2 Completa

--- Financas/Controllers/UsuarioController.cs
+++ Financas/Controllers/UsuarioController.cs
@@ -1,4 +1,5 @@
-﻿using Financas.Entidades;
+﻿using Financas.DAO;
+using Financas.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +10,13 @@ namespace Financas.Controllers
 {
     public class UsuarioController : Controller
     {
+        public UsuarioDAO usuarioDAO;
+
+        public UsuarioController(UsuarioDAO usuarioDAO)
+        {
+            this.usuarioDAO = usuarioDAO;
+        }
+
         // GET: Usuario
         public ActionResult Form()
         {
@@ -19,12 +27,20 @@ namespace Financas.Controllers
         {
             if(ModelState.IsValid)
             {
-
+                usuarioDAO.Adiciona(usuario);
+                return RedirectToAction("Index");
             }
             else
             {
                 return View("Form", usuario);
             }
+        }
+
+        public ActionResult Index()
+        {
+            IList<Usuario> usuarios = usuarioDAO.Lista();
+
+            return View(usuarios);
         }
 
     }
