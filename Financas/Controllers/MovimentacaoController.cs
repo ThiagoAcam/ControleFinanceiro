392fb74a918598b140b89db4695038788514ee05
Thiago Campos de Oliveira <thiago.oliveira@gmail.com>
2016-07-08 19:26:30 -0300

    Aula 3 - Completa

--- Financas/Controllers/MovimentacaoController.cs
+++ Financas/Controllers/MovimentacaoController.cs
@@ -1,4 +1,5 @@
 ﻿using Financas.DAO;
+using Financas.Entidades;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -25,5 +26,27 @@ namespace Financas.Controllers
 
             return View();
         }
+
+        public ActionResult Adiciona(Movimentacao movimentacao)
+        {
+            if(ModelState.IsValid)
+            {
+                movimentacaoDADO.Adiciona(movimentacao);
+                return RedirectToAction("Index", "Movimentacao");
+            }
+            else
+            {
+                ViewBag.Usuarios = usuarioDAO.Lista();
+                return View("Form", movimentacao);
+            }
+        }
+
+        public ActionResult Index()
+        {
+            IList<Movimentacao> movimentacoes = movimentacaoDADO.Lista();
+
+            return View(movimentacoes);
+        }
+
     }
 }
